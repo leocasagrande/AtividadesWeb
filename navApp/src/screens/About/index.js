b8defd9647c8f8cc2c313ccb2b34fdb181e1eaf2
LEONARDO CASAGRANDE E SILVA <leonardo.casagrande@a.ucb.br>
2024-11-12 11:12:18 -0300

    atualizacoes

--- navApp/src/screens/About/index.js
+++ navApp/src/screens/About/index.js
@@ -1,12 +1,18 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
+import { useRoute } from "@react-navigation/native";
+
 const About = ({navigation}) => {
+    const route = useRoute();
     return(
         <View style={styles.container}>
             <Text>About</Text>
             <Button title="Go to Home" onPress={() => navigation.goBack()} />
             <Button title="Go to Detail" onPress={() => navigation.navigate('Detail')} />
+            <Text>Name: {route.params.name}</Text>
+            <Text>Name: {route.params.email}</Text>
+            {console.log(route)}
         </View>
     );
 };
